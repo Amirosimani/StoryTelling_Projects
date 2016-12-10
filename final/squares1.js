adab5218a788f3b0a8be63811162b21440079c44
Amir Imani <Amirosimani@users.noreply.github.com>
2016-12-10 12:24:13 -0500

    fixed squares

--- final/squares1.js
+++ final/squares1.js
@@ -1,7 +1,7 @@
 (function() {
-  var margin = {top: 50, right: 100, bottom: 10, left: 10},
-    width = 400 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+  var margin = {top: 10, right: 10, bottom: 10, left: 10},
+    width = 600 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
   var svg = d3.select("#squares")
         .append("svg")
@@ -11,7 +11,7 @@
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   d3.queue()
-    .defer(d3.csv, "data/listings.csv", function(d, i) {
+    .defer(d3.csv, "data/listings_dummy.csv", function(d, i) {
       d.index = i;
       return d;
     })
@@ -19,43 +19,25 @@
 
   function ready (error, datapoints) {
 
-    svg.selectAll(".hosts")
+    svg.selectAll(".billionaire")
       .data(datapoints, function(d) {
+        // this is a UNIQUE IDENTIFIER for
+        // that particular row so that
+        // d3 can keep track of it
         return d.index
       })
-      .enter().append("circle")
-      .attr("r", 2)
-      .attr("cy", function(d, i) {
-        return 5 * parseInt(i / 200);
+      .enter().append("rect")
+      .attr("height", 15)
+      .attr("width", 15)
+      .attr("y", function(d, i) {
+        return 20 * parseInt(i / 20);
       })
-      .attr("cx", function(d, i) {
-        return 5 * (i % 200);
+      .attr("x", function(d, i) {
+        return 20 * (i % 20);
       })
+      .attr("fill", "#c4cfcf")
 
-    d3.select("#reverse-svg").on('click', function() {
-      // reverse all of our data points
-      var reversed = datapoints.reverse()
 
-      // rebind the data
-      // then use 'i' to reposition them with their new index
-      // the point that was 10,000 is now 0, and 0 is now 10,000
-      svg.selectAll("circle")
-        .data(reversed, function(d) {
-          // this is a UNIQUE IDENTIFIER for
-          // that particular row so that
-          // d3 can keep track of it
-          return d.index
-        })
-        .transition()
-        .duration(2500)
-        .attr("cy", function(d, i) {
-          return 5 * parseInt(i / 200);
-        })
-        .attr("cx", function(d, i) {
-          return 5 * (i % 200);
-        })
-
-    })
-  }
+    }
 
 })();
